7fa7a0e0a169582060fd432d2c46e76bf23eda54
leonardo75v <leonardo75v@yandex.ru>
2018-03-26 13:29:53 +0300

    клака 2

--- js/tamplate-script.js
+++ js/tamplate-script.js
@@ -176,20 +176,7 @@ window.onload = function() {
   document.body.appendChild(css);
 };
 
- $(document).ready(function() {
-  // external js: masonry.pkgd.js, imagesloaded.pkgd.js
 
-// init Masonry
-var $grid = $('.grid').masonry({
-  itemSelector: '.grid-item',
-  percentPosition: true,
-  columnWidth: '.grid-sizer'
-});
-// layout Masonry after each image loads
-$grid.imagesLoaded().progress( function() {
-  $grid.masonry();
-});  
-});
 
 
 
